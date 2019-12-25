0c255a41b97746c7e0dd9b37a84a7d63ca8ee009
GalZilca <galzilca@gmail.com>
2019-12-25 17:08:55 +0200

    no message

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { Dashborad } from "./components/Dashborad";
+import { Dashboard } from "./components/Dashboard";
 import { LogIn } from "./components/LogIn";
 import { SignUp } from "./components/SignUp";
 import { FormAddEvent } from "./components/FormAddEvent";
@@ -14,7 +14,7 @@ class App extends Component {
     return (
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={Dashborad}/>
+          <Route exact path="/" component={Dashboard}/>
           <Route exact path="/logIn" component={LogIn}/>
           <Route exact path="/signUp" component={SignUp}/>
           <Route exact path="/formAddEvent" component={FormAddEvent}/>
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,16 +3,17 @@ import React from "react"
 import { Typography } from "@material-ui/core"
 import BraveTogether from '../images/BraveTogether.png'
 
-export const Dashborad = () => {
+export const Dashboard = () => {
     return (
         <div>
-            <Typography style={{ marginLeft: 80, marginTop: 60}} variant="h3"
+            <Typography style={{ marginLeft: 80, marginTop: 60, display: "inline-block" }} variant="h3"
                 >Brave together
             </Typography>
+            <img style={{ height: "50px", width: "50px", marginTop: 60, display: "inline-block" }} src={BraveTogether} alt="logo" />
             <Typography style={{ marginLeft: 80, marginTop: 60 }} variant="h5"
                 >The Brave together ...
             </Typography>
-            <img style={{ height: "100px", width: "100px", marginTop: 60, marginLeft: 600 }} src={BraveTogether} alt="logo" />
+            
         </div>
     )
 }
