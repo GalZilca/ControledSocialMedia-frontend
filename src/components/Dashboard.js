39d3205ec1fd02d4c9c5e881b5c45b536ea10c3c
GalZilca <galzilca@gmail.com>
2019-12-26 12:54:51 +0200

    no message

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,36 +1,47 @@
 
-import React from "react"
+import React, { useState } from "react"
 import { Link } from 'react-router-dom';
-import { Typography, BottomNavigation, BottomNavigationAction } from "@material-ui/core"
+import { Typography, BottomNavigationAction, Container, Grid } from "@material-ui/core"
+import { ChooseActionPage } from "./ChooseActionPage"
 import BraveTogether from '../images/BraveTogether.png'
 import LogIn from "../images/LogIn.png"
 import SignUp from "../images/SignUp.png"
 
 export const Dashboard = () => {
-    return (
-        <div>
-            <BottomNavigation style={{ backgroundColor: "gray", height: 90 }}>
-                <Link to="/logIn">
-                    <BottomNavigationAction style={{ color: "white", justifyContent: "right" }} label="Log in" icon={
-                        <img style={{ height: "50px", width: "50px" }} src={LogIn}/>}
-                    />
-                </Link>
+    const [user, setUser] = useState({name: "guest", credits: "0"})
 
-                <Link to="/signUp">     
-                    <BottomNavigationAction style={{ color: "white" }} label="Sign Up" icon={
-                        <img style={{ height: "50px", width: "50px" }} src={SignUp}/>}
-                    />
-                </Link>
-            </BottomNavigation>
+    return (
+        <Container style={{ backgroundColor: "gray", height: 90 }}>
+            <Grid style={{ height: 90, flexWrap: "inherit" }} container direction="row" justify="space-around" alignItems="stretch">
+                <p style={{ width: 450, color: "white", marginTop: 31.5 }}>
+                    Hello {user.name}, currently you have {user.credits} credits.
+                </p>
+
+                <Grid style={{ height: 90, flexWrap: "inherit" }} container direction="row" justify="flex-end" alignItems="stretch">
+                    <Link to="/logIn">
+                        <BottomNavigationAction style={{ color: "white", justifyContent: "right" }} label="Log in" icon={
+                            <img style={{ height: "50px", width: "50px" }} src={LogIn}/>}
+                        />
+                    </Link>
+
+                    <Link to="/signUp">     
+                        <BottomNavigationAction style={{ color: "white" }} label="Sign Up" icon={
+                            <img style={{ height: "50px", width: "50px" }} src={SignUp}/>}
+                        />
+                    </Link>
+                </Grid>
+            </Grid>
             
-            <Typography style={{ marginLeft: 80, marginTop: 60, display: "inline-block" }} variant="h3"
-                >Brave together
-            </Typography>
-            <img style={{ height: "50px", width: "50px", marginTop: 60, marginLeft: 400, display: "inline-block" }} src={BraveTogether} alt="logo" />
-            <Typography style={{ marginLeft: 80, marginTop: 40 }} variant="h5"
+            <Grid style={{ marginTop: 40 }} container direction="row" justify="space-around" alignItems="stretch">
+                <Typography /*style={{ marginLeft: 60 }}*/ variant="h3"
+                    >Brave together
+                </Typography>
+                <img style={{ height: "72px", width: "72px", /*marginRight: 60*/ }} src={BraveTogether} alt="logo" />
+            </Grid>
+            <Typography style={{ marginLeft: 60, marginTop: 20 }} variant="h5"
                 >The Brave together ...
             </Typography>
-            
-        </div>
+            <ChooseActionPage/>
+        </Container>
     )
 }
