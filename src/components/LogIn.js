c5fc992007f6c2e81ae9b7a34eb75a3cd24cc75e
GalZilca <galzilca@gmail.com>
2019-12-26 14:36:03 +0200

    no message

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react"
 import { Typography } from "@material-ui/core"
-import BraveTogether from '../images/BraveTogether.png'
 
 import { Link } from 'react-router-dom';
 
@@ -11,9 +10,10 @@ import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+
+import axios from 'axios';
 
 function showError(text,phoneBool,passBool) {
     let error = document.querySelector('#errorText');
@@ -64,84 +64,74 @@ const useStyles = makeStyles(theme => ({
   }));
 
 export const LogIn = () => {
-    //const [user, setUser] = useState("")
-    const [phone,setPhone] = useState("");
-    const [password,setpassword] = useState("");
+  // const [user, setUser] = useState({name: "guest", credits: "0", isManager: 0})
+  const [manager, setManager] = useState(false)
+  const [phone,setPhone] = useState("");
+  const [password,setpassword] = useState("");
 
-    const SubmitLogInForm = (event) => {
-        clearError();
-        let flag = 0;
-        if (phone.length!=10) {
-            showError("Phone must be 10 digits long",1,0);
-            flag = 1;
-        }
-
-        if (password.length<8) {
-            showError("Password must be more than 8 letters long",0,1);
-            flag = 1;
-        }
-
-        if (flag === 0) {
-            // async () => {
-                const user = {phone, password}
-
-                const response = fetch("/login", {
-                    method: "POST",
-                    headers: {
-                        "Content_Type": "application/json"
-                    },
-                    body: JSON.stringify(user)
-                }).catch(e => {
-                    console.log(e)
-                })
-    
-                if (response.ok) {
-                    console.log(response)
-                }
-            // }
-        }
-    
-        // event.preventDefault();
+  const SubmitLogInForm = (event) => {
+    clearError();
+    let flag = 0;
+    if (phone.length!==10) {
+      showError("Phone must be 10 digits long",1,0);
+      flag = 1;
     }
 
-    let classes = useStyles();
+    if (password.length<8) {
+      showError("Password must be more than 8 letters long",0,1);
+      flag = 1;
+    }
     
-    return (
-        <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Log in
-        </Typography>
-        <form className={classes.form}>
-          <TextField variant="outlined" margin="normal" required fullWidth id="phone" label="Phone Number"
-            name="phone" autoComplete="phone" autoFocus 
-            onChange={e => (setPhone(e.target.value), clearError())}/>
-          <TextField variant="outlined" margin="normal" required fullWidth name="password" label="Password"
-            type="password" id="password" autoComplete="current-password" 
-            onChange={e => (setpassword(e.target.value), clearError())}/>
-          <FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Remember me" />
-          <p id="errorText" style={{transition:"1s" , height:"0px", color:"red",display:"flexbox", padding:"0 0 0 0", margin:"5px 0 0 0", textAlign:"center"}}></p>
+    if (flag === 0) {
+      axios.post("/login", { phone, password }).then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
+    } else {
+      event.preventDefault();
+    }
+  }
+
+  let classes = useStyles();
+  
+  return (
+      <Container component="main" maxWidth="xs">
+    <CssBaseline />
+    <div className={classes.paper}>
+      <Avatar className={classes.avatar}>
+      </Avatar>
+      <Typography component="h1" variant="h5">
+        Log in
+      </Typography>
+      <form className={classes.form}>
+        <TextField variant="outlined" margin="normal" required fullWidth id="phone" label="Phone Number"
+          name="phone" autoComplete="phone" autoFocus 
+          onChange={e => (setPhone(e.target.value), clearError())}/>
+        <TextField variant="outlined" margin="normal" required fullWidth name="password" label="Password"
+          type="password" id="password" autoComplete="current-password" 
+          onChange={e => (setpassword(e.target.value), clearError())}/>
+        <FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Remember me" />
+        <p id="errorText" style={{transition:"1s" , height:"0px", color:"red",display:"flexbox", padding:"0 0 0 0", margin:"5px 0 0 0", textAlign:"center"}}></p>
+        <Link to="/">
           <Button onClick={SubmitLogInForm} fullWidth variant="contained" color="primary" 
-          className={classes.submit}>
+            className={classes.submit}>
             Log In
           </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link to="/Need to fill this" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link to="/SignUp" variant="body2">
-                {"Don't have an account? Log Up"}
-              </Link>
-            </Grid>
+        </Link>
+        <Grid container>
+          <Grid item xs>
+            <Link to="/Need to fill this" variant="body2">
+              Forgot password?
+            </Link>
           </Grid>
-        </form>
-      </div>
-    </Container>
-    )
+          <Grid item>
+            <Link to="/SignUp" variant="body2">
+              {"Don't have an account? Log Up"}
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </div>
+  </Container>
+  )
 }
